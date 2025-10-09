4b6a1fb3bf98f865af277b1e03bbe51b39e41713
Mina Kadis <minakadiswadie@gmail.com>
2025-10-09 19:30:46 +0300

    Added Product Queries

---
+++ services/catalog/Catalog.Application/Handlers/Queries/GetProductsByBrandQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+
+using Catalog.Application.Queries;
+using Catalog.Application.Responses;
+using Catalog.Core.Repositories;
+
+using MediatR;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Application.Handlers.Queries
+{
+    public class GetProductsByBrandQueryHandler : IRequestHandler<GetProductsByBrandQuery, IList<ProductResponseDto>>
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly IMapper _mapper;
+
+        public GetProductsByBrandQueryHandler(IMapper mapper, IProductRepository productRepository)
+        {
+            _mapper = mapper;
+            _productRepository = productRepository;
+        }
+
+        public async Task<IList<ProductResponseDto>> Handle(GetProductsByBrandQuery request, CancellationToken cancellationToken)
+        {
+            var productList = await _productRepository.GetAllProductsByBrand(request.BrandName);
+            var productResponseList = _mapper.Map<IList<ProductResponseDto>>(productList);
+            return productResponseList;
+        }
+    }
+}
