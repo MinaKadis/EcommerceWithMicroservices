61ab5b45c6436bb2e848c83a990c8b19ead3db5c
Mina Kadis <minakadiswadie@gmail.com>
2025-10-10 12:27:13 +0300

    Add Product Commands

---
+++ services/catalog/Catalog.Application/Handlers/Commands/CreateProductCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+
+using Catalog.Application.Commands;
+using Catalog.Application.Responses;
+using Catalog.Core.Entities;
+using Catalog.Core.Repositories;
+
+using MediatR;
+
+namespace Catalog.Application.Handlers.Commands
+{
+    public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, ProductResponseDto>
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly IMapper _mapper;
+
+        public CreateProductCommandHandler(IProductRepository productRepository, IMapper mapper)
+        {
+            _productRepository = productRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<ProductResponseDto> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            var product = _mapper.Map<Product>(request);
+            var newProduct = await _productRepository.CreateProduct(product);
+            var productResponse = _mapper.Map<ProductResponseDto>(newProduct);
+
+            return productResponse;
+        }
+    }
+}
